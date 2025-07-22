c2582e4dda862b21e7d7fa78c65fc603cbdec39f
T A Srinivas <tasrinivass@gmail.com>
2025-07-22 12:05:45 +0530

    Update resume_helper.py

--- utils/resume_helper.py
+++ utils/resume_helper.py
@@ -1,1 +1,13 @@
+def build_resume_prompt(resume_text: str, job_desc: str) -> str:
+    return f"""
+You are an expert career coach and recruiter.
+
+Given the following resume and job description, suggest tailored improvements, skills to highlight, and rewrite bullet points if necessary.
+
+Resume:
+{resume_text}
+
+Job Description:
+{job_desc}
+"""
 

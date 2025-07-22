3f86ee07acc7ee3917a6afd5873009ebc35149d7
T A Srinivas <tasrinivass@gmail.com>
2025-07-22 12:06:35 +0530

    Update dream_ai.py

--- utils/dream_ai.py
+++ utils/dream_ai.py
@@ -1,1 +1,9 @@
+def build_dream_prompt(dream_description: str) -> str:
+    return f"""
+You are an expert dream interpreter.
 
+Interpret the symbolic and emotional meaning of this dream:
+
+Dream:
+{dream_description}
+"""

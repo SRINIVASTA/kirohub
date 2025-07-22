205c03078c74cc17981d1c9ea14446b2785bfad9
T A Srinivas <tasrinivass@gmail.com>
2025-07-22 12:06:03 +0530

    Update ai_game.py

--- utils/ai_game.py
+++ utils/ai_game.py
@@ -1,1 +1,12 @@
+def build_game_prompt(current_story: str, player_action: str) -> str:
+    return f"""
+You are the narrator of a text adventure game.
 
+Current story:
+{current_story}
+
+Player action:
+{player_action}
+
+Continue the story in a vivid, engaging way with consequences and new options.
+"""
